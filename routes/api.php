3bfe5ff9c589f8463b3a609fa333633ce5ba369a
Lourdilene Souza <lourdilene.souza@gmail.com>
2022-09-16 11:24:19 -0300

    criação do endpoint de comment

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::apiResource('post', PostController::class);
+
+Route::apiResource('comment', CommentController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
